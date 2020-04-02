6327f065639c3d6e45c09024c81c3d862992c98f
Jay Wen <jaywen89@gmail.com>
2020-04-02 15:55:50 -0400

    adds bootstrap

---
+++ good-space/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>WOOPTY DOO</h2>
+      <Button variant="primary">Log In</Button>{' '}
+    </div>
+  );
+}
+
+export default App;
